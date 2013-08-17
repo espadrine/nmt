b758ac2474d58bd6b5a8df2651355204b9118aa7
Thaddee Tyl <thaddee.tyl@gmail.com>
2013-08-17 22:34:48 +0200

    Update terrain code server-side.

--- terrain.js
+++ terrain.js
@@ -1,7 +1,9 @@
 var SimplexNoise = require('simplex-noise');
+var MersenneTwister = require('./mersenne-twister');
 
-var simplex1 = new SimplexNoise();
-var simplex2 = new SimplexNoise();
+var prng = new MersenneTwister(0);
+var simplex1 = new SimplexNoise(prng.random.bind(prng));
+var simplex2 = new SimplexNoise(prng.random.bind(prng));
 
 // Parameter to how stretched the map is.
 var factor = 50;
@@ -11,61 +13,118 @@ var water = 0;
 var steppe = 1;
 var hills = 2;
 var mountain = 3;
-var altitude = {
+var swamp = 4;
+var meadow = 5;
+var forest = 6;
+var taiga = 7;
+var tileTypes = {
   water: water,
   steppe: steppe,
   hills: hills,
   mountain: mountain,
+  swamp: swamp,
+  meadow: meadow,
+  forest: forest,
+  taiga: taiga
 };
 
-// Get information about the tile at coordinates `coord`.
-// Returns
-//  - height: altitude level. See `altitude`.
-//  - vegetation: boolean; whether there is vegetation.
-function tile(coord) {
-  var x = coord.x;
-  var y = coord.y;
-  var heightNoise = Math.sin(
-      - 4 * Math.abs(simplex1.noise2D(1/4*x/factor, 1/4*y/factor))
-      + simplex1.noise2D(x/factor, y/factor)
-      - 1/2 * Math.abs(simplex1.noise2D(2*x/factor, 2*y/factor))
-      + 1/4 * Math.abs(simplex1.noise2D(4*x/factor, 4*y/factor))
-      - 1/8 * Math.abs(simplex1.noise2D(8*x/factor, 8*y/factor))
-      + 1/16 * Math.abs(simplex1.noise2D(16*x/factor, 16*y/factor)));
-  var seaNoise = 2*simplex2.noise2D(x/factor/2, y/factor/2);
-  var vegetationNoise = simplex2.noise2D(x/factor, y/factor)
-      + 1/2 * simplex2.noise2D(2*x/factor, 2*y/factor)
-      + 1/4 * simplex2.noise2D(4*x/factor, 4*y/factor)
-      + 1/8 * simplex2.noise2D(8*x/factor, 8*y/factor)
-      + 1/16 * simplex2.noise2D(16*x/factor, 16*y/factor);
-  var height = ((heightNoise < -0.99 || heightNoise + seaNoise < -1.7) ?
-                water:
-               (heightNoise < -0.5) ?
-                steppe:
-               (heightNoise < 0.4) ?
-                hills:
-                mountain);
-  var vegetation = vegetationNoise-seaNoise/18+Math.abs(heightNoise+0.15) < 0;
-
-  return {
-    height: height,
-    vegetation: vegetation
-  };
-}
-
 // For each altitude level, [plain name, vegetation name].
-var nameFromTile = {};
-nameFromTile[water]    = ["water", "swamp"];
-nameFromTile[steppe]   = ["steppe", "meadow"];
-nameFromTile[hills]    = ["hills", "forest"];
-nameFromTile[mountain] = ["mountain", "taiga"];
+var nameFromTile = [];
+nameFromTile[water]    = "water";
+nameFromTile[steppe]   = "steppe";
+nameFromTile[hills]    = "hills";
+nameFromTile[mountain] = "mountain";
+nameFromTile[swamp]    = "swamp";
+nameFromTile[meadow]   = "meadow";
+nameFromTile[forest]   = "forest";
+nameFromTile[taiga]    = "taiga";
 
 // Input a tile, output the tile name.
-function tilename(tile) {
-  return nameFromTile[tile.height][+tile.vegetation];
+function tileName(tile) {
+  return nameFromTile[tile.type];
+}
+
+
+var tileVegetationTypeFromSteepness = [];
+tileVegetationTypeFromSteepness[water] = swamp;
+tileVegetationTypeFromSteepness[steppe] = meadow;
+tileVegetationTypeFromSteepness[hills] = forest;
+tileVegetationTypeFromSteepness[mountain] = taiga;
+
+function tileType(steepness, vegetation) {
+  if (vegetation) { return tileVegetationTypeFromSteepness[steepness]; }
+  else { return steepness; }
+}
+
+// (Sparse) Array of array of tiles.
+var memoizedTiles = [];
+
+// Get information about the tile at hexagonal coordinates `coord` {q, r}.
+// Returns
+//  - steepness: altitude level. See `tileTypes`.
+//  - vegetation: boolean; whether there is vegetation.
+//  - type: tile type. See `tileTypes`.
+//  - rain: floating point number between -1 and 1, representing how heavy the
+//  rainfall is.
+function tile(coord) {
+  var q = coord.q;
+  var r = coord.r;
+  if (memoizedTiles[q] != null && memoizedTiles[q][r] != null) {
+    return memoizedTiles[q][r];
+  }
+  var size = simplex2.noise2D(r/500, q/500) * 5;
+  var heightNoise = Math.sin(
+      - (size) * Math.abs(simplex1.noise2D(1/4*q/factor, 1/4*r/factor))
+      + simplex1.noise2D(q/factor, r/factor)
+      - 1/2 * Math.abs(simplex1.noise2D(2*q/factor, 2*r/factor))
+      + 1/4 * Math.abs(simplex1.noise2D(4*q/factor, 4*r/factor))
+      - 1/8 * Math.abs(simplex1.noise2D(8*q/factor, 8*r/factor))
+      + 1/16 * Math.abs(simplex1.noise2D(16*q/factor, 16*r/factor)));
+  var riverNoise = Math.sin(
+      - 16 * Math.abs(simplex1.noise2D(q/16/factor, r/16/factor))
+      + 8 * Math.abs(simplex1.noise2D(q/8/factor, r/8/factor))
+      - 4 * Math.abs(simplex1.noise2D(q/4/factor, r/4/factor))
+      + 2 * Math.abs(simplex1.noise2D(q/2/factor, r/2/factor))
+      - 1/2 * Math.abs(simplex1.noise2D(2*q/factor, 2*r/factor))
+      + 1/4 * Math.abs(simplex1.noise2D(4*q/factor, 4*r/factor))
+      - 1/8 * Math.abs(simplex1.noise2D(8*q/factor, 8*r/factor)));
+  var seaNoise = (size / 2) * simplex1.noise2D(r/factor/8, q/factor/8)
+      + 1/2 * simplex1.noise2D(2*r/factor/8, 2*q/factor/8);
+  var vegetationNoise = (size / 5) * simplex2.noise2D(q/factor, r/factor)
+      + 1/2 * simplex2.noise2D(2*q/factor, 2*r/factor)
+      + 1/4 * simplex2.noise2D(4*q/factor, 4*r/factor)
+      + 1/8 * simplex2.noise2D(8*q/factor, 8*r/factor)
+      + 1/16 * simplex2.noise2D(16*q/factor, 16*r/factor);
+  var steepness =
+    // Rivers are thinner in mountains.
+    ((riverNoise < -0.99 - (heightNoise * 0.013)
+    // Seas are smaller in mountains.
+    || heightNoise + seaNoise < -1.3) ?
+        water:
+    (heightNoise < 0.1) ?
+        steppe:
+    // Mountains are cut off (by hills) to avoid circular mountain formations.
+    (heightNoise < 1 - (riverNoise * 0.42)) ?
+        hills:
+        mountain);
+  var vegetation = (vegetationNoise
+      - (steepness === water? 2 * seaNoise: 0)   // Less vegetation on water.
+      + Math.abs(heightNoise + 0.15)) < 0;
+
+  var tile = {
+    steepness: steepness,
+    vegetation: vegetation,
+    type: tileType(steepness, vegetation),
+    rain: -vegetationNoise / 2
+  };
+  if (memoizedTiles[q] == null) {
+    memoizedTiles[q] = [];
+  }
+  memoizedTiles[q][r] = tile;
+  return tile;
 }
 
 exports.tile = tile;
-exports.altitude = altitude;
-exports.tilename = tilename;
+exports.tileType = tileType;
+exports.tileName = tileName;
 
