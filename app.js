fdcefcca9879ec523544fc2a1b7d1173c949f459
Thaddee Tyl <thaddee.tyl@gmail.com>
2013-10-23 09:22:47 +0200

    Redirect from /not-my-territory/*.

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ var game = require('./game');
 
 game.start(camp);
 
+// Mirror on /not-my-territory/*.
+camp.route(/^\/not-my-territory(\/.*)$/, function (query, match, end) {
+  if (match[1].charCodeAt(match[1].length-1) === 47) {
+    match[1] += 'index.html';   // Directory index.
+  }
+  end(null, { template: match[1] });
+});
+
