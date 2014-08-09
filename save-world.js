97863066cfe9607443e91a2abb33d7cd10c9d728
Thaddee Tyl <thaddee.tyl@gmail.com>
2014-08-09 14:40:44 +0200

    Fixed accidental removal of world saver.

---
+++ save-world.js
@@ -0,0 +1,49 @@
+// Manage the world save and all humanity.
+
+var fs = require('fs');
+var Humanity = require('./humanity.js');
+
+function World(worldFile) {
+  this.humanity = new Humanity();
+  // FIXME: create an instance of terrain.
+  this.terrain = this.humanity.terrain;
+  this.worldFile = worldFile;
+
+  try {
+    var world = require(this.worldFile);
+    this.humanity.patch(world);
+  } catch(e) {
+    this.humanity.setSpawn();
+  }
+  //for (var i = 0; i < this.humanity.numberOfCamps; i++) {
+  //  console.log('camp', i + ':', this.humanity.camps[i].spawn);
+  //}
+  // Update periodically.
+  var periodicity = 10000;  // Every 10s.
+  setInterval(this.saveWorld.bind(this), periodicity);
+}
+
+World.prototype = {
+  humanity: null,
+  terrain: null,
+  worldFile: 'world.json',
+
+  saveWorld: function saveWorld() {
+    // Like a hero.
+    if (this.humanity.dirtyWorld) {
+      var world = {};
+      var humanityData = this.humanity.humanityData;
+      for (var tileKey in humanityData) {
+        world[tileKey] = humanityData[tileKey];
+      }
+      world.places = this.humanity.places;
+      world.centerTile = this.humanity.centerTile;
+      world.campNames = this.humanity.campNames();
+      world.resources = this.humanity.getUsedResources();
+      fs.writeFile(this.worldFile, JSON.stringify(world));
+      this.humanity.dirtyWorld = false;
+    }
+  },
+};
+
+module.exports = World;
